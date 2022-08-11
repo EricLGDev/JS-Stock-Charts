c18794a1421ceae9cb75280ebc525917553c4910
Eric <ericlg.link@gmail.com>
2022-08-11 14:44:55 -0700

    Made a fetch request to twelvedata.com

--- public/index.js
+++ public/index.js
@@ -4,6 +4,15 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch ('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=67297517deb14cddb87eea28d8f60155');
+
+    const result = await response.json();
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    stocks.forEach(stock => stock.values.reverse());
 }
 
 main()
