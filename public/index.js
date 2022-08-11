ca645572aa35279518b79bac30f71b1b2c4eb228
Eric <ericlg.link@gmail.com>
2022-08-11 15:10:36 -0700

    Added node modules and ran a test to make sure timeChart is displayed properly

--- public/index.js
+++ public/index.js
@@ -27,6 +27,20 @@ async function main() {
     const stocks = [GME, MSFT, DIS, BNTX];
 
     stocks.forEach(stock => stock.values.reverse());
+
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    });
 }
 
 main()
